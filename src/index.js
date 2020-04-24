7562d6896d05191f476f6ccf2897fafe28f07de2
alexhruzd <alex.gaby77@gmail.com>
2020-04-24 22:01:02 +0300

    clear project, create 3 components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const AppHeader = () => {
+  return <h1>Todo List</h1>
+};
+
+const SearchBlock = () => {
+  return <input placeholder='search' />;
+};
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn Js</li>
+      <li>Learn Reacy</li>
+    </ul>
+  );
+};
+
+const el = (
+  <div>
+    <AppHeader />
+    <SearchBlock />
+    <TodoList />
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(el, document.getElementById('root'));
+
