9ef32dc1666e7ba434e6a14ef7c94a7aa9de5d37
alexhruzd <alex.gaby77@gmail.com>
2020-05-13 14:56:21 +0300

    make class components

--- src/components/SearchBlock/SearchBlock.js
+++ src/components/SearchBlock/SearchBlock.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './SearchBlock.css';
+import ItemStatusFilter from '../ItemStatusFilter';
 
 const SearchBlock = () => {
     const searchBlockText = 'What are you want to search?'
     return (
         <div className="search-block">
             <input placeholder={searchBlockText} />
+            <ItemStatusFilter />
         </div>
     );
 };
--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -3,27 +3,50 @@ import React from 'react';
 import './TodoListItem.css';
 
 
-const TodoListItem = ({ label, important }) => {
-    const style = {
-        color: important ? 'tomato' : 'black',
+class TodoListItem extends React.Component {
+
+    state = {
+        done: true
+    };
+
+    onLabelClick = () => {
+        console.log(this.props.label);
     }
 
-    return (
-        <div className="todo-list-item">
-            <span
-                className="todo-list-label"
-                style={style}
-            >
-                {label}
-            </span>
-            <button className="btn btn-outline-success btn-important">
-                <i className="fa fa-star" aria-hidden="true"></i>
-            </button>
-            <button className="btn btn-outline-danger">
-                <i className="fa fa-trash" aria-hidden="true"></i>
-            </button>
-        </div>
-    );
+
+    render() {
+
+        const { label, important } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-label';
+        if (done) {
+            classNames += ' done';
+        }
+
+            const style = {
+                color: important ? 'tomato' : 'black',
+            }
+
+        return (
+            <div className="todo-list-item">
+                <span
+                    className="todo-list-label"
+                    style={style}
+                    onClick = {this.onLabelClick}
+                >
+                    {label}
+                </span>
+                <button className="btn btn-outline-success btn-important">
+                    <i className="fa fa-star" aria-hidden="true"></i>
+                </button>
+                <button className="btn btn-outline-danger">
+                    <i className="fa fa-trash" aria-hidden="true"></i>
+                </button>
+            </div>
+        );
+
+    }
 }
 
 export default TodoListItem;
